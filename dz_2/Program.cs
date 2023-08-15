f5249d3561ce725a200f3e380dc85cb0254144c4
Maria Chernomazova <chernomazova@mari.bk>
2023-08-15 21:53:01 +0500

    Решили вторую задачу

---
+++ dz_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значения M и N. Напишите рекурсивный метод, который найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int GetSumOfNatural(int m, int n, int sum = 0) {
+    if (m > n) 
+        return sum;
+    sum = sum + m;
+    return GetSumOfNatural(m+1,n, sum);
+}
+
+Console.WriteLine("Сумма натуральных чисел в промежутке от {0} до {1} равна {2}", m, n, GetSumOfNatural(m,n));
